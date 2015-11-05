8196757c889564caec2a620be12692d4b042c497
jacpy <jacpy.may@gmail.com>
2015-11-05 16:19:58 +0800

    add sdk init class

---
+++ apk/InitHelper.java
@@ -0,0 +1,41 @@
+package com.comtop.mim.helper;
+
+import android.app.Application;
+
+import com.alibaba.mobileim.YWAPI;
+import com.comtop.mim.app.App;
+import com.comtop.mim.utils.LoginHelper;
+
+/**
+ * SDK 全局初始化
+ * 
+ * @author shuheng
+ */
+public class InitHelper {
+
+	public static void initYWSDK(Application application){
+		//TODO 注意：--------------------------------------
+		//  以下步骤调用顺序有严格要求，请按照示例的步骤（todo step）
+		// 的顺序调用！
+		//TODO --------------------------------------------
+
+		// ------[todo step1]-------------
+		//［IM定制初始化］，如果不需要定制化，可以去掉此方法的调用
+		//注意：由于增加全局初始化，该配置需最先执行
+
+		CustomSampleHelper.initCustom();
+
+		// ------[todo step2]-------------
+		//SDK初始化
+		LoginHelper.init(application, App.APP_KEY);
+//		LoginSampleHelper.getInstance().initSDK_Sample(application);
+
+		//后期将使用Override的方式进行集中配置，请参照YWSDKGlobalConfigSample
+		YWAPI.setEnableCrashHandler(false);
+		YWAPI.enableSDKLogOutput(true);
+
+//		IYWContactService.enableBlackList();
+
+//		YWAPI.setEnableAutoLogin(false);
+	}
+}
